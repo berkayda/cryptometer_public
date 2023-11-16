1c55735bbadc4f261dcc904d95f8aaccb7f24124
berkay <87997618+berkayda@users.noreply.github.com>
2023-11-16 22:35:55 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests, datetime
+from pycryptometer import Cryptometer
+c = Cryptometer("YOURAPIKEY")
+#https://pypi.org/project/pycryptometer/#description
+#syntax = c.ANY_OF_THE_CATEGORIES.ANY_OF_THE_FUNCTIONS(THE_ARGS_IT_NEEDS)
+import singleTicker, mergedVolumeToday, tickerList, mergedOrderbook,\
+    rapidMovements, trendIndıcator, liquidationsBTC, liquidationsBitmex
+#import BINANCE_MarketList, BTCfutures, openInterest, tradeVolumeToday
